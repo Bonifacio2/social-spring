9a50858546ad1d0b2054b34dfe2a6abc4c361722
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2018-10-30 18:19:41 -0300

    add project scaffold

---
+++ src/main/java/com/socialspring/socialspring/SocialSpringApplication.java
@@ -0,0 +1,12 @@
+package com.socialspring.socialspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialSpringApplication.class, args);
+	}
+}
